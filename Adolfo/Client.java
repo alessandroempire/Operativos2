d1b884cb7d7af083b19c0de1f4eb1fe247f40e39
Alessandro <alessandroempire@gmail.com>
2015-06-25 18:34:52 -0430

    agregando el buscar

--- Adolfo/Client.java
+++ Adolfo/Client.java
@@ -1,8 +1,15 @@
 //aca va la implementacion del Client
 //java Client -h 127.0.0.1 -p 20771
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
 import java.net.MalformedURLException;
+import java.net.NetworkInterface;
 import java.rmi.*;
+import java.util.Enumeration;
 
 public class Client
 {
@@ -49,7 +56,80 @@ public class Client
 		System.exit(0);
 
 	}
+        
+        
+	public String descubrir_schd() {
+		
+		// Find the server using UDP broadcast
+	int sec = 0;
+	String IP = "";
+	while (sec<=10) {
+		try {
+			//Open a random port to send the package
+			DatagramSocket c = new DatagramSocket();
+			c.setBroadcast(true);
 
+			byte[] sendData = "1".getBytes();
 
+			// Broadcast the message over all the network interfaces
+			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+			while (interfaces.hasMoreElements()) {
+				NetworkInterface networkInterface = interfaces.nextElement();
+
+				if (networkInterface.isLoopback() || !networkInterface.isUp()) {
+					continue; // Don't want to broadcast to the loopback interface
+				}
+
+				for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
+					InetAddress broadcast = interfaceAddress.getBroadcast();
+					if (broadcast == null) {
+						continue;
+					}
+					// Send the broadcast package!
+					try {
+						System.out.println("enviando " );
+						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcast, 9159);
+						c.send(sendPacket);
+					} catch (Exception e) {
+					}				
+				}
+			}
+			//Wait for a response
+			byte[] recvBuf = new byte[1];
+			DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
+			try {
+				c.setSoTimeout(1000);
+				c.receive(receivePacket);
+				
+				//We have a response
+				System.out.println(getClass().getName() + ">>> Broadcast response from server: " + receivePacket.getAddress().getHostAddress());
+				IP = receivePacket.getAddress().getHostAddress();
+				//Close the port!
+				c.close();
+				return IP;
+			} 		catch (Exception e) {
+					}				
+			} catch (IOException ex) {
+				System.out.println("Trouble: " + ex);
+			}
+			try {
+			 Thread.sleep(1000);
+			 sec++;
+			}catch (Exception e) {
+				System.out.println("Problema: " + e );
+			}
+	}
+        
+	System.out.println("No hay schedulers activos, sere uno: " );
+	
+//        L1 = new listener();
+//	daemon = new comprobador_sched();
+//	try {
+//	s.convertir_sched();
+//	}catch (Exception e) {
+//				System.out.println("Problema: " + e );
+//	}
+	return null;
+	}
 
 }
